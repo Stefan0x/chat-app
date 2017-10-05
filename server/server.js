46a08a931a3a991ed2424e376c395d27dd29d676
Stefan Euchenhofer <stefan.euchenhofer@gmail.com>
2017-10-05 21:11:02 +0200

    Add Socket.io

--- server/server.js
+++ server/server.js
@@ -1,18 +1,31 @@
-// Build-in node module to make the path finding easier
+// Build-in node modules
 // https://nodejs.org/dist/latest-v8.x/docs/api/path.html#path_path_join_paths
-const path = require('path');
+const path = require('path'); // make the path finding easier
+const http = require('http');
 
 // Third party node modules
 const express = require('express');
+const socketIO = require('socket.io');
 
 // Custom variables
 const publicPath = path.join(__dirname, '../public');
-var app = express();
 const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 // Express Config
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+// Event listener
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', function(){
+    console.log('Lost connection to the user');
+  });
+});
+
+server.listen(port, () => {
   console.log('Server started on port ' + port)
 })
