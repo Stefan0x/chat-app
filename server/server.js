4e7e0241193da12408ef2dab242e51fa39ddd65a
Stefan Euchenhofer <stefan.euchenhofer@gmail.com>
2017-10-11 18:53:47 +0200

    Add jQuery Form

--- server/server.js
+++ server/server.js
@@ -25,9 +25,10 @@ io.on('connection', (socket) => {
   socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));
   socket.broadcast.emit('newMessage', generateMessage('Admin','New user joined'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('New Message: ', message);
     io.emit('newMessage', generateMessage(message.from,message.text));
+    callback('This is from the server');
   });
 
   socket.on('disconnect', function(){
